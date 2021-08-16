67f7187b388ddc80a346e51416bfd5ea9b36507f
massidiqfattah <massidiqfattah@gmail.com>
2021-08-16 16:49:42 +0700

    max

--- app.py
+++ app.py
@@ -286,11 +286,11 @@ def prediction(filepath):
     res = []   
     if classes[0][0] > classes[0][1]:
         result = classes[0][0] * 100
-        res.append(("Bukan Penyakit Blas",result,img_array,img,relu,conv1_pw_rl,conv2_pw_rl,conv3_pw_rl,conv4_pw_rl,conv5_pw_rl,conv6_pw_rl,conv7_pw_rl,conv8_pw_rl,conv9_pw_rl,conv10_pw_rl,conv11_pw_rl,conv12_pw_rl,conv13_pw_rl,gap,dropout,dense,timing,gmp))
+        res.append(("Bukan Penyakit Blas",result,img_array,img,relu,conv1_pw_rl,conv2_pw_rl,conv3_pw_rl,conv4_pw_rl,conv5_pw_rl,conv6_pw_rl,conv7_pw_rl,conv8_pw_rl,conv9_pw_rl,conv10_pw_rl,conv11_pw_rl,conv12_pw_rl,conv13_pw_rl,gap,dropout,dense,timing,gmp,dropo,dens))
         # print("Daun Padi Sehat")
     elif classes[0][1] > classes[0][0]:
         result = classes[0][1] * 100
-        res.append(("Penyakit Blas Daun Padi",result,img_array,img,relu,conv1_pw_rl,conv2_pw_rl,conv3_pw_rl,conv4_pw_rl,conv5_pw_rl,conv6_pw_rl,conv7_pw_rl,conv8_pw_rl,conv9_pw_rl,conv10_pw_rl,conv11_pw_rl,conv12_pw_rl,conv13_pw_rl,gap,dropout,dense,timing,gmp))
+        res.append(("Penyakit Blas Daun Padi",result,img_array,img,relu,conv1_pw_rl,conv2_pw_rl,conv3_pw_rl,conv4_pw_rl,conv5_pw_rl,conv6_pw_rl,conv7_pw_rl,conv8_pw_rl,conv9_pw_rl,conv10_pw_rl,conv11_pw_rl,conv12_pw_rl,conv13_pw_rl,gap,dropout,dense,timing,gmp,dropo,dens))
         # print("Penyakit Blas Daun Padi")
     return res
 
@@ -317,7 +317,7 @@ def upload_file():
                     basepath, 'static/uploads', secure_filename(f.filename))
                 f.save(file_path)
                 pred = prediction(file_path)
-                return jsonify(res=str(pred[0][0]), acc=str(pred[0][1]), cit=str(pred[0][2]), proc=str(pred[0][3]), conv=str(pred[0][4]), dept1=str(pred[0][5]), dept2=str(pred[0][6]), dept3=str(pred[0][7]), dept4=str(pred[0][8]), dept5=str(pred[0][9]), dept6=str(pred[0][10]), dept7=str(pred[0][11]), dept8=str(pred[0][12]), dept9=str(pred[0][13]), dept10=str(pred[0][14]), dept11=str(pred[0][15]), dept12=str(pred[0][16]), dept13=str(pred[0][17]), gap=str(pred[0][18]), drop=str(pred[0][19]), dense=str(pred[0][20]), timing=pred[0][21], gmp=str(pred[0][22]))
+                return jsonify(res=str(pred[0][0]), acc=str(pred[0][1]), cit=str(pred[0][2]), proc=str(pred[0][3]), conv=str(pred[0][4]), dept1=str(pred[0][5]), dept2=str(pred[0][6]), dept3=str(pred[0][7]), dept4=str(pred[0][8]), dept5=str(pred[0][9]), dept6=str(pred[0][10]), dept7=str(pred[0][11]), dept8=str(pred[0][12]), dept9=str(pred[0][13]), dept10=str(pred[0][14]), dept11=str(pred[0][15]), dept12=str(pred[0][16]), dept13=str(pred[0][17]), gap=str(pred[0][18]), drop=str(pred[0][19]), dense=str(pred[0][20]), timing=pred[0][21], gmp=str(pred[0][22]), dro=str(pred[0][23]), den=str(pred[0][24]))
         else:
             flash('No selected file')
             return redirect(request.url)
